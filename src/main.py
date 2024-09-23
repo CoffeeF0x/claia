23ace8d374da51c22a1dc7945012e0fe6bbf9411
CoffeeF0x_ <lloydtimothybower@gmail.com>
2024-09-23 11:38:16 -0500

    added todo to main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 # - create new characters or update existing (need to move characters to json files)
 # - add modes + add agent mode
 # - add openai streaming support
+# - models should be chosen by the user via key, and passed to the ai via another key pair in its definition
 # - add ability to rename conversations, and perhaps have ai name conversations automatically
 # - create an option to enable a server that serves and updates md files, and sync conversations to md files
 # - Needs a way to filter models (since there are lots) (model list partname?)
