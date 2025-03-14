98cb56e5027c64558911e33ce0034cc9d74c47b6
Coffee <lloydtimothybower@gmail.com>
2025-03-13 20:53:25 -0500

    fixed deploy script

--- src/commands/massedcompute.py
+++ src/commands/massedcompute.py
@@ -133,11 +133,13 @@ STARTUP_SCRIPTS = {
     "sudo apt install -y nano htop",
 
     # Add SSH key
-    "echo \"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGfSWokLXy/QmagyiG6hjPG/wxFKmmgyOk65pLvfNizZ\" > ~/.ssh/authorized_keys",
+    "echo ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGfSWokLXy/QmagyiG6hjPG/wxFKmmgyOk65pLvfNizZ > ~/.ssh/authorized_keys",
 
     # Add user to docker group
     "sudo usermod -aG docker $USER",
-    "newgrp docker",
+
+    # # Restart system
+    # "sudo shutdown -r now",
   ],
   "ddns": [
     "docker run -d --name ddns-updater " +
