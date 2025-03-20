aff2f032bf9cbb2ac9af30bcc55036944c319c2a
Coffee <lloydtimothybower@gmail.com>
2025-03-19 19:28:24 -0500

    added __all__ list for models

--- src/models/transformers/__init__.py
+++ src/models/transformers/__init__.py
@@ -1,3 +1,13 @@
 from models.transformers.base import TransformersModel, TransformersLocalModel, DEFAULT_SETTINGS
 from models.transformers.gemma3 import Gemma3Model, Gemma3LocalModel
 from models.transformers.diffusion import DiffusionModel, DiffusionLocalModel
+
+__all__ = [
+  'TransformersModel',
+  'TransformersLocalModel',
+  'DEFAULT_SETTINGS',
+  'Gemma3Model',
+  'Gemma3LocalModel',
+  'DiffusionModel',
+  'DiffusionLocalModel'
+]
