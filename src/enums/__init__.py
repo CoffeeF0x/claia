98334cf7170e682bf2a50511a52f277f236bf70a
Coffee <lloydtimothybower@gmail.com>
2025-03-30 01:05:27 +0000

    created new logging enum

--- src/enums/__init__.py
+++ src/enums/__init__.py
@@ -2,6 +2,7 @@ from .agent        import ProcessStatus, AgentType, SourcePreference
 from .model        import ModelCapability, IOType
 from .conversation import MessageRole, ActionType, TagType, TagStatus
 from .file         import FileSubdirectory, FileStatus, FileMimeType
+from .logging      import LogLevel, LogFormat
 
 # __all__ = [
 #   'ProcessStatus',
