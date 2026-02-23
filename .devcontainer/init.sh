07ab51442579525dfb6dc164d6b0b10e2b9b6de1
Coffee <lloydtimothybower@gmail.com>
2026-02-23 04:13:03 +0000

    moved build dependencies over to pyproject.toml

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-sudo apt-get update && sudo apt-get install -y build-essential && sudo rm -rf /var/lib/apt/lists/*
+apt-get update
+apt-get install -y build-essential
+rm -rf /var/lib/apt/lists/*
 
-pip install --disable-pip-version-check --no-cache-dir -r build/requirements.txt
-pip install -e .
+pip install --disable-pip-version-check --no-cache-dir -e ".[dev]"
